b1249ddc92caea2bbe04fdafcf8daacc426e45d3
Tommy Chandra <tommy@Tommys-MacBook-Pro.local>
2021-07-17 15:35:03 +0700

    Section 3: List Building - With Style ' '
    Signed-off-by: Tommy Chandra <tommy@Tommys-MacBook-Pro.local>

---
+++ src/screens/ListScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+const ListScreen = () => {
+  const friends = [
+    { name: 'Friend #1', age: 20 },
+    { name: 'Friend #2', age: 45 },
+    { name: 'Friend #3', age: 32 },
+    { name: 'Friend #4', age: 27 },
+    { name: 'Friend #5', age: 53 },
+    { name: 'Friend #6', age: 20 },
+    { name: 'Friend #7', age: 30 },
+    { name: 'Friend #8', age: 20 },
+    { name: 'Friend #9', age: 20 }
+  ]
+
+  return (
+    <FlatList
+      data={friends}
+      keyExtractor={friend => friend.name}
+      renderItem={({ item }) => {
+        return <Text style={styles.textStyle}>{item.name} - Age {item.age}</Text>
+      }}
+    />
+  )
+}
+
+const styles = StyleSheet.create({
+  textStyle: {
+    marginVertical: 50
+  }
+})
+
+export default ListScreen
