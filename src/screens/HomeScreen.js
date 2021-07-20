9872e227b602f5c566bd36153649228e7123559b
Tommy Chandra <tommy@Tommys-MacBook-Pro.local>
2021-07-20 11:50:24 +0700

    Touchable Opacity in Action

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, StyleSheet, View, Button } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
 const HomeScreen = () => {
   return (
@@ -9,6 +9,11 @@ const HomeScreen = () => {
         onPress={() => console.log('Button pressed')}
         title="Go to Components Demo" 
       />
+      <TouchableOpacity onPress={() => console.log('List pressed')} >
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+        <Text>Go to List Demo</Text>
+      </TouchableOpacity>
     </View>
   )
 };
