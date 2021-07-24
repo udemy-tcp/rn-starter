5af91bbd48752d829e32356cd2bf3aee491e9ad7
Tommy Chandra <tommy@Tommys-MacBook-Pro.local>
2021-07-24 15:53:58 +0700

    Reusable Color Adjusters

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,8 +1,15 @@
 import React from "react"
 import { View, StyleSheet, Text } from "react-native"
+import ColorCounter from "../components/ColorCounter"
 
 const SquareScreen = () => {
-  return <Text>Square Screen</Text>
+  return (
+    <View>
+      <ColorCounter color="Red" />
+      <ColorCounter color="Green" />
+      <ColorCounter color="Blue" />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({})
