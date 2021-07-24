c091c19f438decb7afc8869fdd438263dce38850
Tommy Chandra <tommy@Tommys-MacBook-Pro.local>
2021-07-24 15:22:31 +0700

    App Overview

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { View, StyleSheet, Text } from "react-native"
+
+const SquareScreen = () => {
+  return <Text>Square Screen</Text>
+}
+
+const styles = StyleSheet.create({})
+
+export default SquareScreen
