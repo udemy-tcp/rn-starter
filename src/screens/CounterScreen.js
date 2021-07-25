7acd53fa720e7cff758625eaa5b55694b2811ba0
Tommy Chandra <tommy@Tommys-MacBook-Pro.local>
2021-07-25 17:20:55 +0700

    62. Excercise Solution

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,8 +1,26 @@
-import React, { useState } from "react"
-import { View, Text, StyleSheet, Button } from "react-native"
+import React, { useReducer } from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+
+const NUMBER_INCREMENT = 1
+
+const reducer = (state, action) => {
+  // state === { count: number }
+  // action === { type: 'increment' }} 'decrement', payload: NUMBER_INCREMENT }
+
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + action.payload }
+    case 'decrement':
+      return { ...state, count: state.count - action.payload }
+    default:
+      return state
+  }
+}
 
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0)
+  // const [counter, setCounter] = useState(0)
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
+  const { count } = state
 
   return (
     <View>
@@ -12,16 +30,18 @@ const CounterScreen = () => {
           // Don't do this!
           // counter++
           // counter = counter + 1; this is bad implementation!
-          setCounter(counter + 1)
+          // setCounter(counter + 1)
+          // exercise outline to use Reducer instead of useState hook(chapter 61)
+          dispatch({ type: 'increment', payload: NUMBER_INCREMENT })
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter(counter - 1)
+          dispatch({ type: 'decrement', payload: NUMBER_INCREMENT })
         }}
       />
-      <Text>Current Count: {counter}</Text>
+      <Text>Current Count: {count}</Text>
     </View>
   )
 }
