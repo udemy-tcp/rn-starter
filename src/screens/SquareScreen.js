8d5bd8c43de5ac598784c22b39180366ceba1a3a
Tommy Chandra <tommy@Tommys-MacBook-Pro.local>
2021-07-25 12:55:21 +0700

    Creating a Reducer(refactor with reducer hook)

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,56 +1,43 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import ColorCounter from '../components/ColorCounter'
 
 const COLOR_INCREMENT = 15
 
-const SquareScreen = () => {
-  const [red, setRed] = useState(0)
-  const [green, setGreen] = useState(0)
-  const [blue, setBlue] = useState(0)
+const reducer = (state, action) => {
+  // state === { red: 0, green: 0, blue: 0 }
+  // action === { colorToChange: 'red' || 'green' || 'blue', amount: +15, -15 }
 
-  const setColor = (color, change) => {
-    // color === 'red', 'green', 'blue'
-    // change === +15, -15
-
-    switch (color) {
-      case 'red':
-        red + change > 255 || red + change < 0 ? null : setRed(red + change)
-        return
-      case 'green':
-        green + change > 255 || green + change < 0 ? null : setGreen(green + change)
-        return
-      case 'blue':
-        blue + change > 255 || blue + change < 0 ? null : setBlue(blue + change)
-        return
-      default:
-        return
-    }
+  switch (action.colorToChange) {
+    case 'red':
+      return { ...state, red: state.red + action.amount }
+    case 'green':
+      return { ...state, green: state.green + action.amount }
+    case 'blue':
+      return { ...state, blue: state.blue + action.amount }
+    default:
+      return state
   }
+}
+
+const SquareScreen = () => {
+  const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0 })
 
   return (
     <View>
       <ColorCounter
-        onIncrease={() => {
-          // bad implementation because a lot of repeated code and also mixing in a ton of business logic
-          // if (red + COLOR_INCREMENT > 255) {
-          //   return
-          // }
-
-          // better approach for the helper function
-          setColor('red', COLOR_INCREMENT)
-        }}
-        onDecrease={() => setColor('red', -1 * COLOR_INCREMENT)}
+        onIncrease={() => }
+        onDecrease={() => }
         color="Red"
       />
       <ColorCounter
-        onIncrease={() => setColor('green', COLOR_INCREMENT)}
-        onDecrease={() => setColor('green', -1 * COLOR_INCREMENT)}
+        onIncrease={() => }
+        onDecrease={() => }
         color="Green"
       />
       <ColorCounter
-        onIncrease={() => setColor('blue', COLOR_INCREMENT)}
-        onDecrease={() => setColor('blue', -1 * COLOR_INCREMENT)}
+        onIncrease={() => }
+        onDecrease={() => }
         color="Blue"
       />
       <View
