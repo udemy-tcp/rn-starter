a568cd4f3a8b623b8952574b2f0818a5fe66c0a1
Tommy Chandra <tommy@Tommys-MacBook-Pro.local>
2021-07-25 20:10:49 +0700

    63. Handling Text Input

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,33 +1,37 @@
-import React from "react"
-import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native"
+import React from 'react'
+import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native'
 
 const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hi There!</Text>
       <Button
-        onPress={() => navigation.navigate("Components")}
+        onPress={() => navigation.navigate('Components')}
         title="Go to Components Demo"
       />
       <Button
-        onPress={() => navigation.navigate("List")}
+        onPress={() => navigation.navigate('List')}
         title="Go to List Demo"
       />
       <Button
-        onPress={() => navigation.navigate("Image")}
+        onPress={() => navigation.navigate('Image')}
         title="Go to Image Demo"
       />
       <Button
-        onPress={() => navigation.navigate("Counter")}
+        onPress={() => navigation.navigate('Counter')}
         title="Go to Counter Demo"
       />
       <Button
-        onPress={() => navigation.navigate("Color")}
+        onPress={() => navigation.navigate('Color')}
         title="Go to Color Demo"
       />
       <Button
-        onPress={() => navigation.navigate("Square")}
+        onPress={() => navigation.navigate('Square')}
         title="Go to Square Demo"
+      />
+      <Button
+        onPress={() => navigation.navigate('Text')}
+        title="Go to Text Demo"
       />
     </View>
   )
@@ -35,8 +39,8 @@ const HomeScreen = ({ navigation }) => {
 
 const styles = StyleSheet.create({
   text: {
-    fontSize: 30,
-  },
+    fontSize: 30
+  }
 })
 
 export default HomeScreen
